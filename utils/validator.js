bf2e52e866190cf4a5a7de0effb86531e2f9b837
Joshua-Intersmart <joshua@intersmart.in>
2025-06-08 20:55:39 +0530

    Changed DB Schema

--- utils/validator.js
+++ utils/validator.js
@@ -1324,7 +1324,6 @@ const validateServiceEnquiryUpdate = [
 
 const validateServiceType = [
   check("type_name").notEmpty().withMessage("Type name is required"),
-  check("type_value").notEmpty().withMessage("Type Value is required"),
   check("description").optional().isString().withMessage("Description must be a string"),
 ];
 
