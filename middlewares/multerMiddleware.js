1c346487050c4124384cf39b43d022e4c7ca71d6
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2026-08-01 13:29:41 +0530

    adjust file size limit in multer middleware to 50MB

--- middlewares/multerMiddleware.js
+++ middlewares/multerMiddleware.js
@@ -28,7 +28,7 @@ const createUploadMiddleware = (subfolder) => {
   return multer({
     storage: getStorage(subfolder),
     limits: {
-      fileSize: 20 * 1024 * 1024, // 1MB limit
+      fileSize: 50 * 1024 * 1024, // 50MB limit
     },
     // fileFilter: (req, file, cb) => {
     //   const filetypes = /jpeg|jpg|png|svg|webp|mp4|mov|avi|pdf|xlsx/;
